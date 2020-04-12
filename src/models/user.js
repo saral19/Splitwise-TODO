b9194922bcf17e0d1ef0554eaf5a398db3b493bf
saralk1995 <khandelwal.saral2008@gmail.com>
2020-04-12 20:26:09 +1000

    code upto token creation

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-
-const User = mongoose.model('User',
-{
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const userSchema = new mongoose.Schema({
     userID:
     {
         type:Number,
@@ -18,7 +18,65 @@ const User = mongoose.model('User',
     photoUrl:
     {
         type:String
-    }
+    },
+    email:
+    {
+        type:String,
+        unique:true,
+        required:true,
+        trim:true,
+        lowercase:true
+    },
+    password:
+    {
+        type:String,
+        required:true,
+        trim:true
+    },
+    tokens:
+    [
+        {
+            token:
+            {
+                type:String,
+                required:true
+            }
+        }
+    ]
 
 })
+userSchema.pre('save',async function(next)
+{
+    const user = this
+    if(user.isModified('password'))
+    {
+        user.password = await bcrypt.hash(user.password,8)
+    }   
+    next()
+})
+userSchema.statics.findByCredential = async (email,password) =>
+{
+    const user = await User.findOne({email})
+    if(!user)
+    {
+        throw new Error('Unable to login wrong email id')
+    }
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch)
+    {
+        throw new Error("Password is wrond")
+
+    }
+    return user
+}
+userSchema.methods.generateAuthToken = async function()
+{
+    const user = this
+    const token = jwt.sign({_id:user.id.toString()},'SplitwiseTodo')
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
+const User = mongoose.model('User',userSchema)
+
 module.exports = User
--- src/router/user.js
+++ src/router/user.js
@@ -1,17 +1,36 @@
 const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
-
+const auth = require('../middleware/auth')
 router.post('/user',async (req,res) =>
 {
     const user = new User(req.body)
     try{
         await user.save()
-        res.send(201).send(user)
+        const token = await user.generateAuthToken()
+        res.send(201).send({user,token})
     }
     catch(e)
     {
         res.status(400).send(e)
     }
+})
+
+router.post('/users/login',async (req,res) =>
+{
+    try
+    {
+        const user = await User.findByCredential(req.body.email,req.body.password)
+        const token = await user.generateAuthToken()
+        res.send(user)
+    }
+    catch(e)
+    {
+        res.status(400).send()
+    }
+})
+router.get('/users/me',auth,async (req,res) =>
+{   
+    res.send(req.user)          
 })
 module.exports = router
